b8807a5dcf3b531f880702e86e848d09b0825c10
lucasferreiralimax <lucasferreiralimax@gmail.com>
2025-10-07 00:40:46 -0300

    fix: initial project with c# and asp.net

---
+++ src/savemoney/Models/Usuario.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace savemoney.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "O nome é obrigatório.")]
+        public required string Nome { get; set; }
+        [Required(ErrorMessage = "O e-mail é obrigatório.")]
+        public required string Email { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatório.")]
+        [DataType(DataType.Password)]
+        public required string Senha { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime DataAtualizacao { get; set; }
+        [Required(ErrorMessage = "O perfil é obrigatório.")]
+        public Perfil Perfil { get; set; }
+    }
+
+    public enum Perfil {
+        Administrador,
+        UsuarioComum
+    }
+}
