3c6f2310b9bfa4efb8bf56b6fb91f5a6d6fafd2d
Maicon Theodoro <maicon.vitorsilva@hotmail.com>
2025-12-04 05:23:04 -0300

    feat(R7): adiciona enum TipoContabil para classificacao contabil das categorias

---
+++ src/savemoney/Models/Enums/TipoContabil.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace savemoney.Models.Enums
+{
+    /// <summary>
+    /// Classificação contábil da categoria.
+    /// Usado para organização e cálculos financeiros (DRE, relatórios).
+    /// </summary>
+    public enum TipoContabil
+    {
+        [Display(Name = "Não Classificado")]
+        NaoClassificado = 0,
+
+        [Display(Name = "Receita")]
+        Receita = 1,
+
+        [Display(Name = "Custo Variável")]
+        CustoVariavel = 2,
+
+        [Display(Name = "Despesa Fixa")]
+        DespesaFixa = 3,
+
+        [Display(Name = "Despesa Operacional")]
+        DespesaOperacional = 4,
+
+        [Display(Name = "Investimento")]
+        Investimento = 5
+    }
+}
