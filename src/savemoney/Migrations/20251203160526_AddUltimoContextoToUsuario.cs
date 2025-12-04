da5b2e9d007aacc3414c61f1efc6279098b26b5c
Maicon Theodoro <maicon.vitorsilva@hotmail.com>
2025-12-04 00:04:22 -0300

    feat(db): migration para adicionar UltimoContexto na tabela Usuario

---
+++ src/savemoney/Migrations/20251203160526_AddUltimoContextoToUsuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace savemoney.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUltimoContextoToUsuario : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UltimoContexto",
+                table: "Usuario",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DataCadastro", "UltimoContexto" },
+                values: new object[] { new DateTime(2025, 12, 3, 13, 5, 26, 103, DateTimeKind.Local).AddTicks(4024), 0 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UltimoContexto",
+                table: "Usuario");
+
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataCadastro",
+                value: new DateTime(2025, 12, 2, 1, 46, 27, 167, DateTimeKind.Local).AddTicks(1916));
+        }
+    }
+}
