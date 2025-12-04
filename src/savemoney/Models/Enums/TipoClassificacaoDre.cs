95e5591b7734b6d76e346020b69cab78f7a4e31d
Maicon Theodoro <maicon.vitorsilva@hotmail.com>
2025-12-04 03:57:37 -0300

    feat(R7): adiciona enums TipoRegimeDre e TipoClassificacaoDre para DRE Gerencial

---
+++ src/savemoney/Models/Enums/TipoClassificacaoDre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace savemoney.Models.Enums
+{
+    /// <summary>
+    /// Classificação da categoria para cálculo do DRE Gerencial.
+    /// Define se a categoria representa custo variável ou despesa operacional.
+    /// </summary>
+    public enum TipoClassificacaoDre
+    {
+        [Display(Name = "Não Classificado")]
+        NaoClassificado = 0,
+
+        [Display(Name = "Custo Variável")]
+        CustoVariavel = 1,
+
+        [Display(Name = "Despesa Operacional")]
+        DespesaOperacional = 2
+    }
+}
