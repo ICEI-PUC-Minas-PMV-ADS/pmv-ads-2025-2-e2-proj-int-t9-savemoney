aac675886c6e40b527376340fe1ec6a710be4b54
Maicon Theodoro <maicon.vitorsilva@hotmail.com>
2025-12-04 03:01:25 -0300

    fix(conversor): corrige erro 500 adicionando coluna UsuarioId no banco e validação no controller

---
+++ src/savemoney/Migrations/20251204053145_AddUsuarioIdToConversor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace savemoney.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUsuarioIdToConversor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataCadastro",
+                value: new DateTime(2025, 12, 4, 2, 31, 45, 63, DateTimeKind.Local).AddTicks(3983));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Usuario",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "DataCadastro",
+                value: new DateTime(2025, 12, 3, 13, 5, 26, 103, DateTimeKind.Local).AddTicks(4024));
+        }
+    }
+}
